72d455c963f9cf1344f7cbf0a0e11a83f548a52c
jamestagal <benjaminjameswaller@gmail.com>
2022-08-02 21:52:39 +1000

    add link to style sheet

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -14,4 +14,5 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   <link rel="icon" type="image/svg+xml" href="/assets/logo.svg">
   <link rel='stylesheet' href='/spa/bundle.css'>
+  <link rel='stylesheet' href='/assets/global.css'>
 </head>
