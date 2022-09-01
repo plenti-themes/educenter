9ed5f8dcbaa125a747eab87a479c3510c1e0b264
jimafisk <jimafisk@gmail.com>
2022-09-01 16:32:50 -0400

    Copy styles from demo site.

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -11,7 +11,9 @@
   <script type="module" src="/spa/ejected/main.js"></script>
 <!-- CSS only -->
 <link rel="stylesheet" href="/assets/themify-icons/themify-icons.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<!--
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+-->
   <link rel="icon" type="image/svg+xml" href="/assets/logo.svg">
   <link rel='stylesheet' href='/spa/bundle.css'>
   <link rel='stylesheet' href='/assets/global.css'>
