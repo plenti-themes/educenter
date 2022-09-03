9ebf0e88a3298af6f31499e466403880d25061d6
jamestagal <benjaminjameswaller@gmail.com>
2022-09-03 14:47:21 +1000

    update post.svelte

--- layouts/content/posts.svelte
+++ layouts/content/posts.svelte
@@ -1,6 +1,20 @@
 <script>
     export let title, body, image, author, publish, category;
 </script>
+<section class="page-title-section overlay" style="background-image:url(/educenter/site/images/backgrounds/page-title.jpg),url(/educenter/site/images/backgrounds/page-title.jpg)">
+    <div class="container">
+    <div class="row">
+    <div class="col-md-8">
+    <ul class="list-inline custom-breadcrumb">
+    <li class="list-inline-item h2"><a class="text-primary font-secondary" href=".">Home</a></li>
+    <li class="list-inline-item h5"><i class="ti-angle-right text-white"></i></li>
+    <li class="list-inline-item text-white h3 font-secondary">{title}</li>
+    </ul>
+    <p class="text-lighten">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+    </div>
+    </div>
+    </div>
+    </section>
 <section class="section-sm">
     <div class="container">
     <div class="row">
