9477c82db288adb8b4805a23c89cdcab53e4c8e0
jamestagal <benjaminjameswaller@gmail.com>
2022-09-25 20:32:40 +1000

    update news section for dynamic data

--- layouts/components/news.svelte
+++ layouts/components/news.svelte
@@ -1,5 +1,18 @@
 <script>
-    export let title, items;
+    import Posts from "../posts.svelte";
+
+    export let title, allContent;
+    let articles = allContent.filter(content => content.type === "article");
+
+const sortByDate = (items, order) => {
+items.sort((a, b) => { 
+    // Must have field named "date" in content source to work.
+    let dateA = new Date(a?.fields?.publish?.date);
+    let dateB = new Date(b?.fields?.publish?.date);
+    return (order == "oldest") ? (dateA - dateB) : (dateB - dateA);
+});
+return items;
+};
 </script>
 <section class="section">
     <div class="container">
@@ -9,21 +22,23 @@
             </div>
         </div>
         <div class="row justify-content-center">
-            {#each items as item}
+            {#each sortByDate(allContent.filter(content => content.type === "posts")) as post,i}
+                        {#if i < 3}
                 <article class="col-lg-4 col-sm-6 mb-5 mb-lg-0">
                     <article class="card rounded-0 border-bottom border-primary border-top-0 border-left-0 border-right-0 hover-shadow">
-                    <img class="card-img-top rounded-0" src="{item.image.url}" alt="{item.image.alt}">
+                    <img class="card-img-top rounded-0" src="{post.fields.image.source}" alt="{post.fields.image.alt}">
                         <div class="card-body">
                         <ul class="list-inline mb-3">
-                        <li class="list-inline-item mr-3 ml-0"><i class="{item.icon1}"></i>{item.date}</li>
-                        <li class="list-inline-item mr-3 ml-0"><i class="{item.icon2}"></i> <a href="{item.user.url}">{item.user.name}</a></li>
+                        <li class="list-inline-item mr-3 ml-0"><i class="{post.fields.icon.calendar}"></i>{post.fields.publish.date}</li>
+                        <li class="list-inline-item mr-3 ml-0"><i class="{post.fields.icon.user}"></i> <a href="{post.fields.author.link}">{post.fields.author.name}</a></li>
                         </ul>
-                        <h4 class="card-title"><a href="{item.link.url}">{item.title}</a></h4>
-                        <p class="card-text">{item.body}</p>
-                        <a href="{item.link.url}" class="btn btn-primary btn-sm">{item.link.title}</a>
+                        <h4 class="card-title"><a href="{post.path}">{post.fields.link.title}</a></h4>
+                        <p class="card-text">{post.fields.body.substring(0, 150).replace(/(<([^>]+)>)/gi, "")}</p>
+                        <a href="{post.path}" class="btn btn-primary btn-sm">{post.fields.link.title}</a>
                         </div>
                     </article>
                 </article>
+                {/if}
             {/each}
         </div>
     </div>
