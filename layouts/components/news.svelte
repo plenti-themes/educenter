c8cc33a46c15c69d6921135a3a6fbf4ef51abd4b
jamestagal <benjaminjameswaller@gmail.com>
2022-09-26 23:43:56 +1000

    update image path

--- layouts/components/news.svelte
+++ layouts/components/news.svelte
@@ -16,13 +16,13 @@
                         {#if i < 3}
                 <article class="col-lg-4 col-sm-6 mb-5 mb-lg-0">
                     <article class="card rounded-0 border-bottom border-primary border-top-0 border-left-0 border-right-0 hover-shadow">
-                    <img class="card-img-top rounded-0" src="{post.fields.image.source}" alt="{post.fields.image.alt}">
+                    <img class="card-img-top rounded-0" src="/assets{post.fields.image.source}" alt="{post.fields.image.alt}">
                         <div class="card-body">
                         <ul class="list-inline mb-3">
-                        <li class="list-inline-item mr-3 ml-0"><i class="{post.fields.icon.calendar}"></i>{post.fields.publish.date}</li>
+                        <li class="list-inline-item mr-3 ml-0"><i class="{post.fields.icon.calendar}"></i> {post.fields.publish.date}</li>
                         <li class="list-inline-item mr-3 ml-0"><i class="{post.fields.icon.user}"></i> <a href="{post.fields.author.link}">{post.fields.author.name}</a></li>
                         </ul>
-                        <h4 class="card-title"><a href="{post.path}">{post.fields.link.title}</a></h4>
+                        <h4 class="card-title"><a href="{post.path}">{post.fields.title}</a></h4>
                         <p class="card-text">{post.fields.body.substring(0, 150).replace(/(<([^>]+)>)/gi, "")}</p>
                         <a href="{post.path}" class="btn btn-primary btn-sm">{post.fields.link.title}</a>
                         </div>
