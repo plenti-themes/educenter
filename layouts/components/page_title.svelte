53acdfa276228612ea56d679350969959d0ca262
jamestagal <benjaminjameswaller@gmail.com>
2022-10-23 00:48:27 +1100

    create page title component

--- layouts/components/page_title.svelte
+++ layouts/components/page_title.svelte
@@ -1,14 +1,17 @@
-<section class="page-title-section overlay" style="background-image:url(/educenter/site/images/backgrounds/page-title.jpg),url(/educenter/site/images/backgrounds/page-title.jpg)">
+<script>
+    export let title, home, body, image;
+</script>
+<section class="page-title-section overlay" style="background-image:url({image.url}),url({image.url})">
     <div class="container">
-    <div class="row">
-    <div class="col-md-8">
-    <ul class="list-inline custom-breadcrumb">
-    <li class="list-inline-item h2"><a class="text-primary font-secondary" href="https://demo.gethugothemes.com/educenter/site/">Home</a></li>
-    <li class="list-inline-item h5"><i class="ti-angle-right text-white"></i></li>
-    <li class="list-inline-item text-white h3 font-secondary">Our Courses</li>
-    </ul>
-    <p class="text-lighten">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
-    </div>
-    </div>
+        <div class="row">
+            <div class="col-md-8">
+                <ul class="list-inline custom-breadcrumb">
+                <li class="list-inline-item h2"><a class="text-primary font-secondary" href="{home.url}">{home.title}</a></li>
+                <li class="list-inline-item h5"><i class="ti-angle-right text-white"></i></li>
+                <li class="list-inline-item text-white h3 font-secondary">{title}</li>
+                </ul>
+                <p class="text-lighten">{body}</p>
+            </div>
+        </div>
     </div>
 </section>
