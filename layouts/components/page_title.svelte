4301b9650660603cfdaa32070bc12ff808f0e23f
jamestagal <benjaminjameswaller@gmail.com>
2022-10-22 23:10:46 +1100

    create new compenent for page titles

---
+++ layouts/components/page_title.svelte
@@ -0,0 +1,14 @@
+<section class="page-title-section overlay" style="background-image:url(/educenter/site/images/backgrounds/page-title.jpg),url(/educenter/site/images/backgrounds/page-title.jpg)">
+    <div class="container">
+    <div class="row">
+    <div class="col-md-8">
+    <ul class="list-inline custom-breadcrumb">
+    <li class="list-inline-item h2"><a class="text-primary font-secondary" href="https://demo.gethugothemes.com/educenter/site/">Home</a></li>
+    <li class="list-inline-item h5"><i class="ti-angle-right text-white"></i></li>
+    <li class="list-inline-item text-white h3 font-secondary">Our Courses</li>
+    </ul>
+    <p class="text-lighten">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+    </div>
+    </div>
+    </div>
+</section>
