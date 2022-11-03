e8bf820a109ac6a27fc93874b4805e63ca6f283e
jamestagal <benjaminjameswaller@gmail.com>
2022-11-04 01:09:18 +1100

    Create single event_page content type

---
+++ layouts/content/event_page.svelte
@@ -0,0 +1,18 @@
+<section class="section">
+    <div class="container">
+        <div class="row">
+        <div class="col-lg-4 col-sm-6 mb-5">
+        <div class="card border-0 rounded-0 hover-shadow">
+        <div class="card-img position-relative">
+        <img class="card-img-top rounded-0" src="/educenter/site/images/events/event-1.jpg" alt="Social media and behavior economics conference">
+        <div class="card-date"><span>09</span><br>Mar</div>
+        </div>
+        <div class="card-body">
+        <p><i class="ti-location-pin text-primary mr-2"></i>Dhanmondi, Dhaka</p>
+        <h4 class="card-title"><a href="https://demo.gethugothemes.com/educenter/site/event/event-1/">Social media and behavior economics conference</a></h4>
+        </div>
+        </div>
+        </div>
+        </div>
+    </div>
+</section>
