759fc38d33dcba7bb39608ab58dac3edabc58623
jamestagal <benjaminjameswaller@gmail.com>
2022-11-04 13:50:02 +1100

    add header section

--- layouts/content/event_page.svelte
+++ layouts/content/event_page.svelte
@@ -1,3 +1,20 @@
+<script>
+    export let home, image, body, title;
+</script>
+<section class="page-title-section overlay" style="background-image:url({image.url}),url({image.url})">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+            <ul class="list-inline custom-breadcrumb">
+            <li class="list-inline-item h2"><a class="text-primary font-secondary" href="{home.url}">{home.title}</a></li>
+            <li class="list-inline-item h5"><i class="ti-angle-right text-white"></i></li>
+            <li class="list-inline-item text-white h3 font-secondary">{title}</li>
+            </ul>
+            <p class="text-lighten">{body}</p>
+            </div>
+        </div>
+    </div>
+  </section>
 <section class="section">
     <div class="container">
         <div class="row">
