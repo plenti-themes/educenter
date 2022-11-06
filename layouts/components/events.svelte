24cc5e1ffdc8f5e227239caeb21afa46b4b48568
jamestagal <benjaminjameswaller@gmail.com>
2022-11-07 10:18:27 +1100

    fix sytax error

--- layouts/components/events.svelte
+++ layouts/components/events.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { sortByDate } from "../scripts/sortByDate.svelte";}
+    import { sortByDate } from "../scripts/sortByDate.svelte";
     export let title, link, allContent;
 </script>
 <section class="section bg-gray">
