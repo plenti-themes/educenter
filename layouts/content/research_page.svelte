bec92195a72bcc3b0671af28d6a560d5547c5258
jamestagal <benjaminjameswaller@gmail.com>
2022-11-11 09:07:44 +1100

    create Research single page content type

---
+++ layouts/content/research_page.svelte
@@ -0,0 +1,15 @@
+<section class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-6 mb-4">
+            <div class="card rounded-0 hover-shadow border-top-0 border-left-0 border-right-0">
+            <img class="card-img-top rounded-0" src="/educenter/site/images/research/research-1.jpg" alt="Geography">
+            <div class="card-body">
+            <h4 class="card-title"><a href="https://demo.gethugothemes.com/educenter/site/research/research-1/">Geography</a></h4>
+            <p class="card-text">About Research Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor …</p>
+            </div>
+            </div>
+            </div>
+            </div>
+        </div>
+</section>
