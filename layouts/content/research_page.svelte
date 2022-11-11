8fce182a89676f6645188febd7f4aca48a1b0c22
jamestagal <benjaminjameswaller@gmail.com>
2022-11-11 13:07:11 +1100

    add allContent filter & replacement pattern

--- layouts/content/research_page.svelte
+++ layouts/content/research_page.svelte
@@ -1,15 +1,35 @@
+<script>
+    export let title, image, home, body, allContent;
+   
+</script>
+<section class="page-title-section overlay" style="background-image:url({image.url}),url({image.url})">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+            <ul class="list-inline custom-breadcrumb">
+            <li class="list-inline-item h2"><a class="text-primary font-secondary" href="{home.url}">{home.title}</a></li>
+            <li class="list-inline-item h5"><i class="ti-angle-right text-white"></i></li>
+            <li class="list-inline-item text-white h3 font-secondary">{title}</li>
+            </ul>
+            <p class="text-lighten">{body}</p>
+            </div>
+        </div>
+    </div>
+</section>
 <section class="section">
     <div class="container">
         <div class="row">
+            {#each allContent.filter(content => content.type == "research") as research}
             <div class="col-lg-4 col-sm-6 mb-4">
             <div class="card rounded-0 hover-shadow border-top-0 border-left-0 border-right-0">
-            <img class="card-img-top rounded-0" src="/educenter/site/images/research/research-1.jpg" alt="Geography">
+            <img class="card-img-top rounded-0" src="assets{research.fields.image.url}" alt="{research.fields.image.alt}">
             <div class="card-body">
-            <h4 class="card-title"><a href="https://demo.gethugothemes.com/educenter/site/research/research-1/">Geography</a></h4>
-            <p class="card-text">About Research Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor …</p>
+            <h4 class="card-title"><a href="{research.path}">{research.fields.title}</a></h4>
+            <p class="card-text">{research.fields.tbody}</p>
             </div>
             </div>
             </div>
-            </div>
+            {/each}
         </div>
+    </div>
 </section>
