524aa161a0df6591dac8842326f3dceb09fd6841
jamestagal <benjaminjameswaller@gmail.com>
2022-11-30 21:31:42 +1100

    updaet aboutus page

---
+++ layouts/components/aboutus.svelte
@@ -0,0 +1,15 @@
+<script>
+    // import { PageTitle } from '.page_title.svelte';
+    export let title, body, image;
+</script>
+<section class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+            <img class="img-fluid w-100 mb-4" src="{image.url}" alt="{image.alt}">
+            <h2 id="about-our-journy">{title}</h2>
+            <p>{body}</p>
+            </div>
+        </div>
+    </div>
+</section>
