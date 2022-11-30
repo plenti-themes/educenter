abdaf22364ab0f17e3ffc06773ad195b53cfaf17
jamestagal <benjaminjameswaller@gmail.com>
2022-11-30 21:32:05 +1100

    create stats component

---
+++ layouts/components/stats.svelte
@@ -0,0 +1,30 @@
+<section class="section-sm bg-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6 mb-4 mb-md-0">
+                <div class="text-center">
+                <h2 class="count text-white" data-count="60">60</h2>
+                <h5 class="text-white">TEACHERS</h5>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 mb-4 mb-md-0">
+                <div class="text-center">
+                <h2 class="count text-white" data-count="50">50</h2>
+                <h5 class="text-white">COURSES</h5>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 mb-4 mb-md-0">
+                <div class="text-center">
+                <h2 class="count text-white" data-count="1000">1000</h2>
+                <h5 class="text-white">STUDENTS</h5>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 mb-4 mb-md-0">
+                <div class="text-center">
+                <h2 class="count text-white" data-count="1400">1400</h2>
+                <h5 class="text-white">SATISFIED CLIENT</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
