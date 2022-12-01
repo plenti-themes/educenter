79de70b1182334fa1f19a6b01ba8ce2dd5a01650
jamestagal <benjaminjameswaller@gmail.com>
2022-12-02 00:25:15 +1100

    create contact page and component

---
+++ layouts/components/contact.svelte
@@ -0,0 +1,26 @@
+<section class="section bg-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+            <h2 class="section-title">Contact Us</h2>
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-lg-7 mb-4 mb-lg-0">
+            <form action="#" method="post">
+            <input type="text" class="form-control mb-3" id="name" name="name" placeholder="Your Name">
+            <input type="email" class="form-control mb-3" id="mail" name="mail" placeholder="Your Email">
+            <input type="text" class="form-control mb-3" id="subject" name="subject" placeholder="Subject">
+            <textarea name="message" id="message" class="form-control mb-3" placeholder="Your Message"></textarea>
+            <button type="submit" value="send" class="btn btn-primary">send message</button>
+            </form>
+            </div>
+            <div class="col-lg-5 content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit recusandae voluptates doloremque veniam temporibus porro culpa ipsa, nisi soluta minima saepe laboriosam debitis nesciunt. Dolore, labore. Accusamus nulla sed cum aliquid exercitationem debitis error harum porro maxime quo iusto aliquam dicta modi earum fugiat, vel possimus commodi, deleniti et veniam, fuga ipsum praesentium. Odit unde optio nulla ipsum quae obcaecati! Quod esse natus quibusdam asperiores quam vel, tempore itaque architecto ducimus expedita</p>
+            <a href="tel:+211234565523" class="text-color h5 d-block">+211234565523</a>
+            <a href="mailto:info@educenter.com" class="mb-5 text-color h5 d-block">info@educenter.com</a>
+            <p>9567 Turner Trace Apt. BC C3G8A4</p>
+            </div>
+        </div>
+    </div>
+</section>
