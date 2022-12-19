1c9ba96ed47b68a1e288b2fa88528cccd5be8a1e
jamestagal <benjaminjameswaller@gmail.com>
2022-12-19 20:56:41 +1100

    add replacement pattern from json object

--- layouts/components/stats.svelte
+++ layouts/components/stats.svelte
@@ -1,28 +1,31 @@
+<script>
+    export let teachers, courses, students, satisfied;
+</script>
 <section class="section-sm bg-primary">
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-sm-6 mb-4 mb-md-0">
                 <div class="text-center">
-                <h2 class="count text-white" data-count="60">60</h2>
-                <h5 class="text-white">TEACHERS</h5>
+                <h2 class="count text-white" data-count="60">{teachers.amount}</h2>
+                <h5 class="text-white">{teachers.title}</h5>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 mb-4 mb-md-0">
                 <div class="text-center">
-                <h2 class="count text-white" data-count="50">50</h2>
-                <h5 class="text-white">COURSES</h5>
+                <h2 class="count text-white" data-count="50">{courses.amount}</h2>
+                <h5 class="text-white">{courses.title}</h5>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 mb-4 mb-md-0">
                 <div class="text-center">
-                <h2 class="count text-white" data-count="1000">1000</h2>
-                <h5 class="text-white">STUDENTS</h5>
+                <h2 class="count text-white" data-count="1000">{students.amount}</h2>
+                <h5 class="text-white">{students.title}</h5>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 mb-4 mb-md-0">
                 <div class="text-center">
-                <h2 class="count text-white" data-count="1400">1400</h2>
-                <h5 class="text-white">SATISFIED CLIENT</h5>
+                <h2 class="count text-white" data-count="1400">{satisfied.amount}</h2>
+                <h5 class="text-white">{satisfied.title}</h5>
                 </div>
             </div>
         </div>
