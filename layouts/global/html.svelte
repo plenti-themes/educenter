761f16652f5ede06c0827ff0bf79ddbe817f8a5d
jimafisk <jimafisk@gmail.com>
2022-12-27 12:21:14 -0500

    Add CMS.

--- layouts/global/html.svelte
+++ layouts/global/html.svelte
@@ -5,26 +5,37 @@
   import Footer from './footer.svelte';
   import Newsletter from './newsletter.svelte';
   import { makeTitle } from '../scripts/make_title.svelte';
-  export let content, layout, allContent, allLayouts, env;
+  export let content, layout, allContent, allLayouts, env, user, adminMenu;
 </script>
 
 <html lang="en">
   <Head title={makeTitle(content.filename)} {env} />
-<body>
-  <header class="fixed-top header">
-    <div class="top-header py-2 bg-white">
-      <div class="container">
-        <Header />
-      </div>
-    </div>
-  <div class="navigation w-100 top-hider" style="margin-top: 0px;">
-    <div class="container">
-      <Nav />
-    </div>
-  </div>
-</header>
-  <svelte:component this={layout} {...content.fields} {allContent} {allLayouts} {content} />
-  <Newsletter />
-  <Footer />
-</body>
+  <body>
+    {#if user && $user.isAuthenticated}
+        <svelte:component this={adminMenu} {user} bind:content={content} />
+    {/if}
+    <main>
+      <header class="fixed-top header">
+        <div class="top-header py-2 bg-white">
+          <div class="container">
+            <Header {user} />
+          </div>
+        </div>
+        <div class="navigation w-100 top-hider" style="margin-top: 0px;">
+          <div class="container">
+            <Nav />
+          </div>
+        </div>
+      </header>
+      <svelte:component this={layout} {...content.fields} {allContent} {allLayouts} {content} />
+      <Newsletter />
+      <Footer />
+    </main>
+  </body>
 </html>
+
+<style>
+  :global(nav) ~ main header {
+    top: 41px;
+  }
+</style>
