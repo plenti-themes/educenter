6e2d2c1d3c16ff2fd17d5d0375fcc4c307e057b9
jamestagal <benjaminjameswaller@gmail.com>
2023-01-09 16:43:51 +1100

    add slick js import

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -10,7 +10,9 @@
 
   <base href="{ env.baseurl }">
   <script type="module" src="/spa/ejected/main.js"></script>
+  <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <!-- CSS only -->
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 <link rel="stylesheet" href="/assets/themify-icons/themify-icons.css">
 <!--
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
