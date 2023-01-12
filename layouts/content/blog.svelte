258b5c715097bdfa177d3373c13f00c0c6e9ec56
jimafisk <jimafisk@gmail.com>
2023-01-11 20:47:06 -0500

    Fixes for 0.6.x release.

--- layouts/content/blog.svelte
+++ layouts/content/blog.svelte
@@ -65,7 +65,7 @@
                     {#each sortByDate(allContent.filter(content => content.type === "posts")) as post,i}
                         {#if i < 3}
                             <div class="media border-bottom border-color pb-3 mb-3">
-                                <a href="{post.path}}"><img class="mr-3 post-thumb-sm" src="/assets/{post.fields.image.source}" alt="{post.fields.image.alt}"></a>
+                                <a href="{post.path}}"><img class="mr-3 post-thumb-sm" src="/media/{post.fields.image.source}" alt="{post.fields.image.alt}"></a>
                                 <div class="media-body">
                                 <a href="{post.path}">
                                 <h5 class="mt-0">{post.fields.title}</h5>
