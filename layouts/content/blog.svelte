bc681843a2e3398392f703522c35e66a63407ed7
jamestagal <benjaminjameswaller@gmail.com>
2023-02-18 17:25:00 +1100

    remove aside template and import from aside comp

--- layouts/content/blog.svelte
+++ layouts/content/blog.svelte
@@ -1,6 +1,7 @@
 <script>
   import Pagination from "../components/pagination.svelte";
   import LatestArticles from "../components/latest_articles.svelte";
+  import Aside from "../components/aside.svelte";
   export let title, image, header, headerText, allContent, content;
   //   let articles = allContent.filter(content => content.type === "article");
 
@@ -91,50 +92,7 @@
           </div>
         </div>
       </div>
-      <aside class="col-lg-4 order-1 order-lg-2">
-        <div class="bg-white mb-5">
-          <h4 class="mb-4">Categories</h4>
-          {#each allContent.filter(content => content.type == "catgs") as catg}
-          <ul class="list-unstyled">
-            <li class="border-bottom">
-              <a
-                href="{catg.path}"
-                class="d-block pb-3 mt-3">{catg.fields.name}</a
-              >
-            </li>
-          </ul>
-          {/each}
-        </div>
-        <div class="bg-white mb-5">
-          <h4 class="mb-4">Tags</h4>
-          <ul class="list-inline tag-list">
-          {#each allContent.filter(content => content.type == "tags") as tag}
-            <li class="list-inline-item mb-2">
-              <a href="/educenter/site/tags/advice/">{tag.fields.name}</a>
-            </li>
-            {/each}
-          </ul>
-        </div>
-        <LatestArticles {allContent} />
-      </aside>
-      <!--<aside class="col-lg-4 order-1 order-lg-2"> 
-                <div class="bg-white">
-                    <h4 class="mb-4">Latest Articles</h4>
-                    {#each sortByDate(allContent.filter(content => content.type === "posts")) as post,i}
-                        {#if i < 3}
-                            <div class="media border-bottom border-color pb-3 mb-3">
-                                <a href="{post.path}}"><img class="mr-3 post-thumb-sm" src="/media/{post.fields.image.source}" alt="{post.fields.image.alt}"></a>
-                                <div class="media-body">
-                                <a href="{post.path}">
-                                <h5 class="mt-0">{post.fields.title}</h5>
-                                </a>
-                                {post.fields.publish.date}
-                                </div>
-                            </div>
-                        {/if}
-                    {/each}
-                </div>
-            </aside>  -->
+      <Aside {allContent} />
         </div>
     </div>
 </section>
