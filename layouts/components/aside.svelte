e07012a6ee90a85f591a97d1ac6fa5b8809a465a
jimafisk <jimafisk@gmail.com>
2023-03-19 14:04:37 -0400

    Fix broken build.

--- layouts/components/aside.svelte
+++ layouts/components/aside.svelte
@@ -1,22 +1,16 @@
 <script>
     export let  catg, allContent, content, catgPosts;
-    $: catg = {
-    name: content.fields.name,
-    route: content.fields.route,
-    posts: Object(catgPosts.filter((key) => key.name == content.fields.name)[0])
-      .posts,
-  };
 </script>
 
 <aside class="col-lg-4 order-1 order-lg-2">
     <div class="bg-white mb-5">
       <h4 class="mb-4">Categories</h4>
       <ul class="list-unstyled">
-        {#each Object(content => content.type == "blog_catgs") as { path, name }}
-        <li class="border-bottom {(catg ? catg.name : '') == name ? 'active' : ''}" >
+        {#each Object(allContent.filter(content => content.type == "blog_catgs")) as category}
+          <li class="border-bottom {category ? category.fields.name : ''}" >
           <a
-            href={path}
-            class="d-block pb-3 mt-3">{name}</a
+            href={category.path}
+            class="d-block pb-3 mt-3">{category.fields.name}</a
           >
         </li>
         {/each}
