f360163cc8883ba86976b85f6681485e4ea87410
jimafisk <jimafisk@gmail.com>
2023-03-19 14:25:47 -0400

    Blog categories (#17).

--- layouts/content/blog_catgs.svelte
+++ layouts/content/blog_catgs.svelte
@@ -1,10 +1,8 @@
 <script>
     import Aside from "../components/aside.svelte";
     import LatestArticles from "../components/latest_articles.svelte";
-    export let name, header, headerImage, allContent, content, catg, catgPosts;
-    let posts = allContent.filter(
-    (content) =>
-      content.type === "posts" && content.fields.category.name === name)[0];
+    export let name, headerImage, allContent, content, catg, catgPosts;
+    let posts = allContent.filter(content => content.type === "posts");
 </script>
 <section
   class="page-title-section overlay"
@@ -15,9 +13,7 @@
       <div class="col-md-8">
         <ul class="list-inline custom-breadcrumb">
           <li class="list-inline-item h2">
-            <a class="text-primary font-secondary" href={header.url}
-              >{header.title}</a
-            >
+            <a class="text-primary font-secondary" href="/">Home</a>
           </li>
           <li class="list-inline-item h5">
             <i class="ti-angle-right text-white" />
@@ -33,54 +29,52 @@
       <div class="row">
         <div class="col-lg-8 order-2 order-lg-1">
           <div class="row">
-            <!-- {#each allContent.filter(content => content.type == "posts") as post} -->
-              {#if posts}
+              {#each posts as post}
+                {#if post.fields.category.name === name}
                 <div class="col-sm-6 mb-5">
                   <article
                     class="card rounded-0 border-bottom border-primary border-top-0 border-left-0 border-right-0 hover-shadow"
                   >
-                    <a href={posts.path}>
+                    <a href={post.path}>
                       <img
                         class="card-img-top rounded-0"
-                        src={posts.fields.image.source}
-                        alt={posts.fields.image.alt}
+                        src={post.fields.image.source}
+                        alt={post.fields.image.alt}
                       /></a
                     >
                     <div class="card-body">
                       <ul class="list-inline mb-3">
                         <li class="list-inline-item mr-3 ml-0">
                           <i class="ti-calendar" />
-                          {posts.fields.publish.date}
+                          {post.fields.publish.date}
                         </li>
                         <li class="list-inline-item mr-3 ml-0">
                           <i class="ti-user" />
-                          <a href={posts.fields.author.link}
-                            >{posts.fields.author.name}</a
+                          <a href={post.fields.author.link}
+                            >{post.fields.author.name}</a
                           >
                         </li>
                       </ul>
                       <h4 class="card-title">
-                        <a href={posts.path}>{posts.fields.title}</a>
+                        <a href={post.path}>{post.fields.title}</a>
                       </h4>
                       <p class="card-text">
-                        {posts.fields.description
+                        {post.fields.description
                           .substring(0, 150)
                           .replace(/(<([^>]+)>)/gi, "")}
                       </p>
-                      <a href={posts.path} class="btn btn-primary btn-sm"
+                      <a href={post.path} class="btn btn-primary btn-sm"
                         >Read More</a
                       >
                     </div>
                   </article>
                 </div>
-              {/if}
-<!--             <div class="col-12">
-              <Pagination {currentPage} {totalPages} />
-            </div> -->
+                {/if}
+              {/each}
           </div>
         </div>
         <Aside {allContent} {content} {catg} {catgPosts}/>
-        <LatestArticles {allContent} />
+        <!-- <LatestArticles {allContent} /> -->
           </div>
       </div>
   </section>
