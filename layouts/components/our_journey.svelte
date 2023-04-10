7e2fc200ca7cce4688a451329a51522d0d02151f
jamestagal <benjaminjameswaller@gmail.com>
2023-04-10 12:00:27 +1000

    new file name after renaming from aboutus

---
+++ layouts/components/our_journey.svelte
@@ -0,0 +1,15 @@
+<script>
+    // import { PageTitle } from '.page_title.svelte';
+    export let title, description, image;
+</script>
+<section class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+            <img class="img-fluid w-100 mb-4" src="{image.url}" alt="{image.alt}">
+            <h2 id="about-our-journy">{title}</h2>
+            <p>{@html description}</p>
+            </div>
+        </div>
+    </div>
+</section>
