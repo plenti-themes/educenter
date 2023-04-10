20ac7c4851c46c6c71a549840dc8d62c65b32016
jamestagal <benjaminjameswaller@gmail.com>
2023-04-10 12:01:09 +1000

    add replacement pattern from content source

--- layouts/components/contact.svelte
+++ layouts/components/contact.svelte
@@ -1,25 +1,29 @@
+<script>
+    // import { PageTitle } from '.page_title.svelte';
+    export let title, description, contact, button, form;
+</script>
 <section class="section bg-gray">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-            <h2 class="section-title">Contact Us</h2>
+            <h2 class="section-title">{title}</h2>
             </div>
             </div>
             <div class="row">
             <div class="col-lg-7 mb-4 mb-lg-0">
             <form action="#" method="post">
-            <input type="text" class="form-control mb-3" id="name" name="name" placeholder="Your Name">
-            <input type="email" class="form-control mb-3" id="mail" name="mail" placeholder="Your Email">
-            <input type="text" class="form-control mb-3" id="subject" name="subject" placeholder="Subject">
-            <textarea name="message" id="message" class="form-control mb-3" placeholder="Your Message"></textarea>
-            <button type="submit" value="send" class="btn btn-primary">send message</button>
+            <input type="text" class="form-control mb-3" id="name" name="name" placeholder="{form.name}">
+            <input type="email" class="form-control mb-3" id="mail" name="mail" placeholder="{form.email}">
+            <input type="text" class="form-control mb-3" id="subject" name="subject" placeholder="{form.subject}">
+            <textarea name="message" id="message" class="form-control mb-3" placeholder="{form.message}"></textarea>
+            <button type="submit" value="send" class="btn btn-primary">{button.text}</button>
             </form>
             </div>
             <div class="col-lg-5 content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit recusandae voluptates doloremque veniam temporibus porro culpa ipsa, nisi soluta minima saepe laboriosam debitis nesciunt. Dolore, labore. Accusamus nulla sed cum aliquid exercitationem debitis error harum porro maxime quo iusto aliquam dicta modi earum fugiat, vel possimus commodi, deleniti et veniam, fuga ipsum praesentium. Odit unde optio nulla ipsum quae obcaecati! Quod esse natus quibusdam asperiores quam vel, tempore itaque architecto ducimus expedita</p>
-            <a href="tel:+211234565523" class="text-color h5 d-block">+211234565523</a>
-            <a href="mailto:info@educenter.com" class="mb-5 text-color h5 d-block">info@educenter.com</a>
-            <p>9567 Turner Trace Apt. BC C3G8A4</p>
+            <p>{@html description}</p>
+            <a href="tel:+211234565523" class="text-color h5 d-block">{contact.phone}</a>
+            <a href="mailto:info@educenter.com" class="mb-4 text-color h5 d-block">{contact.email}</a>
+            <p>{contact.address}</p>
             </div>
         </div>
     </div>
