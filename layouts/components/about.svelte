dfe11cf54d9f828cadb3d3b629124c56ca22e4c5
jamestagal <benjaminjameswaller@gmail.com>
2023-04-16 22:01:10 +1000

    add link to button in template

--- layouts/components/about.svelte
+++ layouts/components/about.svelte
@@ -7,7 +7,7 @@
             <div class="col-md-6 order-2 order-md-1">
                 <h2 class="section-title">{title}</h2>
                     <p>{@html description}</p>
-                    <a href="{link.url}" class="btn btn-outline-primary">{link.title}</a>
+                    <a href="aboutus" class="btn btn-outline-primary">{link.title}</a>
             </div>
             <div class="col-md-6 order-1 order-md-2 mb-4 mb-md-0">
                 <img class="img-fluid w-100" src="{image.url}" alt="{image.alt}">
