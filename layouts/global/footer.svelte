b47063e113849762d279835e7a13eb3b299462aa
jamestagal <benjaminjameswaller@gmail.com>
2023-04-22 17:10:35 +1000

    add links to footer

--- layouts/global/footer.svelte
+++ layouts/global/footer.svelte
@@ -12,38 +12,38 @@
     <div class="col-lg-2 col-sm-4 col-6 mb-5 mb-md-0">
     <h4 class="text-white mb-5 text-uppercase">company</h4>
     <ul class="list-unstyled">
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/about/">About</a></li>
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/course/">course</a></li>
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/blog/">blog</a></li>
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/contact/">Contact</a></li>
+    <li class="mb-3"><a class="text-color" href="aboutus">About</a></li>
+    <li class="mb-3"><a class="text-color" href="courses">courses</a></li>
+    <li class="mb-3"><a class="text-color" href="blog">blog</a></li>
+    <li class="mb-3"><a class="text-color" href="contactus">Contact</a></li>
     </ul>
     </div>
     <div class="col-lg-2 col-sm-4 col-6 mb-5 mb-md-0">
     <h4 class="text-white mb-5 text-uppercase">links</h4>
     <ul class="list-unstyled">
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/event/">event</a></li>
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/notice/">notice</a></li>
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/research/">research</a></li>
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/scholarship/">scholarship</a></li>
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/teacher/">teacher</a></li>
+    <li class="mb-3"><a class="text-color" href="events">events</a></li>
+    <li class="mb-3"><a class="text-color" href="notices">notices</a></li>
+    <li class="mb-3"><a class="text-color" href="research">research</a></li>
+    <li class="mb-3"><a class="text-color" href="scholarships">scholarships</a></li>
+    <li class="mb-3"><a class="text-color" href="teachers">teachers</a></li>
     </ul>
     </div>
     <div class="col-lg-2 col-sm-4 col-6 mb-5 mb-md-0">
     <h4 class="text-white mb-5 text-uppercase">Courses</h4>
     <ul class="list-unstyled">
-    <li class="mb-3"><a class="text-color" href=".">Artificial Intelligence</a></li>
-    <li class="mb-3"><a class="text-color" href=".">JavaScript</a></li>
-    <li class="mb-3"><a class="text-color" href=".">Philosophy</a></li>
-    <li class="mb-3"><a class="text-color" href=".">Graphic Design</a></li>
-    <li class="mb-3"><a class="text-color" href=".">Photography</a></li>
+    <li class="mb-3"><a class="text-color" href="course/artificial-intelligence">Artificial Intelligence</a></li>
+    <li class="mb-3"><a class="text-color" href="course/javascript">JavaScript</a></li>
+    <li class="mb-3"><a class="text-color" href="course/philosophy">Philosophy</a></li>
+    <li class="mb-3"><a class="text-color" href="course/graphic-design">Graphic Design</a></li>
+    <li class="mb-3"><a class="text-color" href="course/photography">Photography</a></li>
     </ul>
     </div>
     <div class="col-lg-2 col-sm-4 col-6 mb-5 mb-md-0">
     <h4 class="text-white mb-5 text-uppercase">new notice</h4>
     <ul class="list-unstyled">
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/notice/notice-1/">Teaching Materials and Testing Methods</a></li>
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/notice/notice-2/">Teaching Materials and Testing Methods</a></li>
-    <li class="mb-3"><a class="text-color" href="https://demo.gethugothemes.com/educenter/site/notice/notice-3/">Teaching Materials and Testing Methods</a></li>
+    <li class="mb-3"><a class="text-color" href=".">Teaching Materials and Testing Methods</a></li>
+    <li class="mb-3"><a class="text-color" href=".">Teaching Materials and Testing Methods</a></li>
+    <li class="mb-3"><a class="text-color" href=".">Teaching Materials and Testing Methods</a></li>
     </ul>
     </div>
     </div>
